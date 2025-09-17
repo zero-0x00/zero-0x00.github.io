37e8a1f2391ad8e41166cc9765189cae313f682e
zero-0x00 <marvelsrp@yandex.ru>
2025-09-17 10:34:11 +0300

    wip

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -11,13 +11,20 @@ function AboutMe() {
       className={'text-wide'}
     >
       <Box sx={{ mt: 1 }}>
-        Senior Frontend-разработчик с 16+ годами опыта, из них 8.5 года во
-        Frontend. Специализируюсь на React, Typescript, NodeJS, SocketIO, Vite,
-        Webpack. Выступаю на IT-конференциях, занимаюсь менторством, регулярно
-        провожу Code Review и рефакторинг. Внедряю CI/CD, Code Splitting и Lazy
-        Loading. Активно применяю лучшие практики: DRY, YAGNI, KISS, SOLID,
-        SMART. Умею находить баланс между Clean Architecture и Feature-Sliced
-        Design.
+        Senior Frontend-разработчик с 9 годами опыта в создании
+        высоконагруженных SPA-приложений. Специализируюсь на React, TypeScript и
+        Redux, имею опыт разработки серверных микросервисов на Node.js (включая
+        WebSockets/Socket.io).
+      </Box>
+      <Box sx={{ my: 1 }}>
+        Участвую в проектировании архитектуры, планировании спринтов и релизов,
+        провожу регулярные Code review. Настраивал CI/CD пайплайны, внедрял
+        Agile/Scrum процессы в команде.
+      </Box>
+      <Box sx={{ mb: 1 }}>
+        Разрабатывал PWA для iOS/Android, создавал дизайн-системы и интегрировал
+        Storybook. Есть опыт оптимизации производительности и сокращения времени
+        загрузки приложений.
       </Box>
     </SectionAccordion>
   );
